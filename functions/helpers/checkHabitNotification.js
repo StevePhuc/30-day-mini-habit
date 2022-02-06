d1bc62131e57604cc7c692d007c4ce604e5b1601
Steve Phuc <phuc@free.fi>
2022-02-06 11:46:15 +0200

    habit new with help

--- functions/helpers/checkHabitNotification.js
+++ functions/helpers/checkHabitNotification.js
@@ -9,24 +9,24 @@ module.exports = (habit) => {
 
   // for to check each habit
   habit.forEach((item) => {
-    console.log("----- check habit id", item.id);
-    if (isAfter(now, new Date(item.start_date)) && isBefore(now, new Date(item.end_date))) {
-      const isDone = item.habit_track.find(
-        (_item) => _item.date === format(now, "yyyy-MM-dd")
+    console.log("----- check habit id", item?.id);
+    if (isAfter(now, new Date(item?.start_date)) && isBefore(now, new Date(item?.end_date))) {
+      const isDone = item?.habit_track.find(
+        (_item) => _item?.date === format(now, "yyyy-MM-dd")
       )?.done;
       console.log("check done", isDone);
       if (!isDone) {
         const timeNow = now.toISOString().substring(11, 16);
         console.log("send reminder", timeNow);
 
-        const isReminderMorning = timeNow === item.reminder_time.morningReminder.substring(0, 5);
+        const isReminderMorning = timeNow === item?.reminder_time?.morningReminder.substring(0, 5);
         const isReminderAfternoon =
-          timeNow === item.reminder_time.afternoonReminder.substring(0, 5);
-        const isReminderEvening = timeNow === item.reminder_time.eveningReminder.substring(0, 5);
+          timeNow === item?.reminder_time?.afternoonReminder.substring(0, 5);
+        const isReminderEvening = timeNow === item?.reminder_time?.eveningReminder.substring(0, 5);
 
-        const toEmail = item.reminder_type.email;
-        const habitName = item.habit_name;
-        const html = `click here to mark as done <a href = "https://mini-habit-30.netlify.app/api/done?id=${item.id}&user_id=${item.user_id}"> CLICK HERE</a>`;
+        const toEmail = item?.reminder_type.email;
+        const habitName = item?.habit_name;
+        const html = `click here to mark as done <a href = "https://mini-habit-30.netlify.app/api/done?id=${item?.id}&user_id=${item?.user_id}"> CLICK HERE</a>`;
         if (isReminderMorning) {
           console.log("send morning reminder");
           sendEmail({
@@ -53,6 +53,6 @@ module.exports = (habit) => {
         }
       }
     }
-    console.log("done habit id----", item.id);
+    console.log("done habit id----", item?.id);
   });
 };
